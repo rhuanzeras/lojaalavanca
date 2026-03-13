e48b9faf89794a1a6a31cfa71a19b9bc4200cc8d
Rhuan Marques <rhuan243@gmail.com>
2026-03-13 15:55:40 -0300

    adicionando logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main>
-      <h1>LOJA ALAVANCA ONLINE</h1>
+    <main style={{padding:"40px"}}>
+
+      <div style={{display:"flex",alignItems:"center",gap:"15px"}}>
+        <img src="/logo.png" width="60" />
+        <h1>Loja Atlética Alavanca</h1>
+      </div>
+
+      <p>Bem vindo à loja oficial da Atlética.</p>
+
     </main>
-  )
+  );
 }
